595519a621f258845b8302f28623dac9ecdd13a9
ddanelynn <dingyoujia0823@gmail.com>
2022-06-24 00:14:56 +0800

    complete navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component:HomeComponent },
+  { path: 'resume', component:ResumeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
